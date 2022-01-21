96ea211166c036f8137a6f301ff69ea0448802df
thejerremi <thejerremi@gmail.com>
2022-01-21 17:07:18 +0100

    nowa metoda policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+using namespace std;
+int Policz(int a, int b){
+    return a+b;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout <<"W pierwszym branchu: 2+3=" << Policz(2,3) << endl;
     return 0;
 }
